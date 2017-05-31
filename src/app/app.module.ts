582c81b064840e88c2f5e46ef6a052ba7ca5ec4f
gs-server <nitin@darkhorsesports.in>
2017-05-31 19:03:41 +0530

    added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule } from '@angular/router';
+import { routes } from './app.router';
 import { AppComponent } from './app.component';
+import { EventComponent } from './components/event/event.component';
+import { TournamentComponent } from './components/tournament/tournament.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventComponent,
+    TournamentComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routes,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
