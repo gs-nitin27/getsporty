c3fa1a70c9dc78663a39a7c4dc68a0819fce4ee7
nitin <nitin@darkhorsesports.in>
2017-07-11 14:43:08 +0530

    profile check update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,9 +5,9 @@ export let APP_CONFIG = new OpaqueToken("app.config");
 
 export const AppConfig: IAppConfig = {  
 
-     apBaseUrl: "http://localhost/getsporty/php",
+     applocalhostUrl: "http://localhost/getsporty/php",
 
-     applocalhostUrl : "https://getsporty.in/dist/php",
+     apBaseUrl : "http://getsporty.in/dist/assets/php",
 
 
 
---
+++ src/assets/php/config1.php
@@ -0,0 +1,18 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization, Token, token, TOKEN');
+if($_SERVER['REQUEST_METHOD'] == "OPTIONS"){
+    exit();
+}
+$con = mysql_connect('localhost','root','');
+if($con)
+{
+     $selected = mysql_select_db('getsport_staging') or die("Could not select databasename");
+}
+else
+{
+echo "could not connect";
+} 
+
+?>
