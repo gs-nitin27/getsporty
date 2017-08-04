eaa7f5bb8f1ecfdbfb40a22185c7379e2d9f9e79
sagar seth <sagar12jan@gmail.com>
2017-08-04 15:45:31 +0530

    bug api timeout

--- src/app/components/job/createJob/job.component.ts
+++ src/app/components/job/createJob/job.component.ts
@@ -86,6 +86,7 @@ _handleReaderLoaded(readerEvt) {
   var binaryString = readerEvt.target.result;
   this._JobServices.uploadimage(btoa(binaryString)).subscribe( data =>
     {
+         alert(data);
         this.myVar = false;
         if(data != "Null")
         {
